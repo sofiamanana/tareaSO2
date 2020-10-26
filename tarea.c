2f031c831661ee3c47dfa2d2dd0fafb751a8829b
Sofia <sofia.manana@sansano.usm.cl>
2020-10-26 02:53:36 +0000

    probar saltar turno

--- tarea.c
+++ tarea.c
@@ -8,6 +8,7 @@
 struct cuadrado { 
     int efecto;  
 };
+
 void crearTablero(struct cuadrado * tablero){
     for(int i=0; i<29; i++){
         if(i==2 || i==4 || i==6 || i==12 || i==14 || i==21 || i==23 || i==25 || i==27){
@@ -78,19 +79,22 @@ int tirarDado(){
     return num;
 }
 
-void signo1(struct cuadrado *tablero, int *pos, int id){
+int signo1(struct cuadrado *tablero, int *pos, int id){
     int num = rand()%5+1;
     if(num==1){
         //retrocede 1 cuadricula(no sobrepasar el inicio)
+        printf("Retrocedes una casilla\n");
         int n_pos = pos[id]-1;
         if(n_pos<0){
             pos[id]=0;
         }else{
             pos[id]=n_pos;
         }
+        return 1;
     }
     else if(num==2){
         //todos los jugadores (excepto el del turno) retroceden una cuadricula(no sobrepasar el inicio)
+        printf("Todos retroceden una casilla\n");
         for(int i=0; i<4; i++){
             if(id==i){
                 continue;
@@ -104,50 +108,180 @@ void signo1(struct cuadrado *tablero, int *pos, int id){
                 }
             }
         }
+        return 1;
     }
     else if(num==3){
         //el jugador avanza una cuadricula
+        printf("Avanzaste una cuadricula\n");
         int n_pos = pos[id]+1;
         if(n_pos>=28){
             //gano
         }else{
             pos[id]=n_pos;
         }
+        return 1;
     }
     else if(num==4){
         //el siguiente jugador pierde el turno
+        printf("El siguiente jugador pierde el turno\n");
+        return 2;
     }
     else if(num==5){
         //cambia el sentido de los turnos
+        return 1;
     }
 
 }
 
-void signo2(struct cuadrado *tablero, int nuevapos, int id){
-    
-
+int signo2(struct cuadrado *tablero, int *pos, int id){
+    int num = rand()%10+1;
+    if(num==1 || num==2 || num==3){
+        printf("Todos retroceden dos cuadriculas\n");
+        for(int i=0; i<4; i++){
+            int n_pos = pos[i]-2;
+                if(n_pos<0){
+                    pos[i]=0;
+                }else{
+                    pos[i]=n_pos;
+                }
+        }
+        return 1;
+    }
+    else if(num==4 || num==5){
+        printf("Todos avanzan hasta la cuadricula blanca mas cercana\n");
+        for(int i=0; i<4; i++){
+            int flag=0;
+            int avanzar=1;
+            while(flag==0 && (tablero[pos[i]+avanzar].efecto!=4)){
+                if(tablero[pos[i]+avanzar].efecto==0){
+                    flag=1;
+                }
+                else{
+                    avanzar++;
+                }
+            }
+        }
+        return 1;
+    }
+    else if(num==6 || num==7){
+        printf("El jugador cambia de lugar con el jugador que va en el ultimo lugar\n");
+        int flag=0;
+        int aux;
+        int min=pos[id];
+        int lugar=id;
+        for(int i=0; i<4; i++){
+            if(i==id){
+                continue;
+            }
+            else{
+                if(pos[id]>pos[i]){
+                    flag=1;
+                }
+                else if(min>=pos[i]){
+                    min=pos[i];
+                    lugar=i;
+                }
+            }
+        }
+        if(flag==0){
+            return 1;
+        }
+        aux=pos[id];
+        pos[id]=min;
+        pos[lugar]=aux;
+        return 1;
+    }
+    else if(num==8 || num==9){
+        printf("El jugador cambia de lugar con el jugador que va en primer lugar\n");
+        int flag=0;
+        int aux;
+        int max=pos[id];
+        int lugar=id;
+        for(int i=0; i<4; i++){
+            if(i==id){
+                continue;
+            }
+            else{
+                if(pos[id]<pos[i]){
+                    flag=1;
+                }
+                else if(max<=pos[i]){
+                    max=pos[i];
+                    lugar=i;
+                }
+            }
+        }
+        if(flag==0){
+            return 1;
+        }
+        aux=pos[id];
+        pos[id]=max;
+        pos[lugar]=aux;
+        return 1;
+    }
+    else if(num==10){
+        printf("Cambia de sentido el tablero\n");
+        int i;
+        for (i = 0; i<28;i++){
+            if(tablero[i].efecto==1){
+                tablero[i].efecto=2;
+            }
+            else if(tablero[i].efecto==2){
+                tablero[i].efecto=1;
+            }
+        }
+        for(i=0; i<4; i++){
+            pos[i] = 28 - pos[i];
+        }
+        return 1;
+    }
 }
 
-void moverJugador(struct cuadrado *tablero, int *pos, int id){
+int moverJugador(struct cuadrado *tablero, int *pos, int id){
     int dado = tirarDado();
+    printf("Dado: %d\n",dado);
     int pos_jugador = pos[id];
     int nueva_pos = pos_jugador+dado;
     pos[id]=nueva_pos;
+    int enter;
     if(nueva_pos>=28){
         //gano
+        pos[id]=nueva_pos;
+        pos[5]=0;
+        printf("Ganoo...Termino turno, presiones 0 para continuar\n");
+        scanf("%d",&enter);
+        return 0;
     }
     else if(tablero[nueva_pos].efecto==1){
         //1 signo de pregunta-->funcion
+        pos[id]=nueva_pos;
+        int ret = signo1(tablero,pos,id);
+        printf("?...Termino turno, presiones 0 para continuar\n");
+        printf("ret: %d\n",ret);
+        scanf("%d",&enter);
+        return ret;
     }
     else if(tablero[nueva_pos].efecto==2){
         //2 signos de preguntas-->funcion
+        pos[id]=nueva_pos;
+        int ret = signo2(tablero, pos, id);
+        printf("??...Termino turno, presiones 0 para continuar\n");
+        printf("ret: %d\n",ret);
+        scanf("%d",&enter);
+        return ret;
     }
     else if(tablero[nueva_pos].efecto==4){
         pos[5]=0;
+        pos[id]=nueva_pos;
+        printf("Ganooo...Termino turno, presiones 0 para continuar\n");
+        scanf("%d",&enter);
+        return 0;
     }
     else if(tablero[nueva_pos].efecto==0){
         pos[id]=nueva_pos;
-        printf("Termino turno\n");
+        printf("Celda blanca...Termino turno, presiones 0 para continuar\n");
+        scanf("%d",&enter);
+        return 1;
     }
 }
 
@@ -175,7 +309,8 @@ int main(){
     for(int i=0; i<4; i++){
         pos[i]=0;
     }
-    pos[5]=100;
+    pos[5]=100; //flag de salida
+    pos[6]=0;   //flag de saltar turno
 
     //ARREGLAR Y PONER EXCEPCIONES
     int a = pipe(ab);
@@ -195,29 +330,122 @@ int main(){
         }
     }
 
-    if(id==0){
-        printf("soy el proceso padre\n");
+    if(id==0){  //jugador 1
         close(da[1]);
         close(ab[0]);
     }
-    else if(id==1){
-        printf("soy el proceso 1\n");
+    else if(id==1){  //jugador 2
         close(ab[1]);
         close(bc[0]);
     }
-    else if(id==2){
-        printf("soy el proceso 2\n");
+    else if(id==2){  //jugador 3
         close(bc[1]);
         close(cd[0]);
     }
-    else if(id==3){
-        printf("soy el proceso 3\n");
+    else if(id==3){  //jugador 4
         close(cd[1]);
         close(da[0]);
     }
 
-    
-    fclose(fp);
-    fclose(fp2);
+    int k=0;
+
+    while(pos[5] == 100){
+        if(id==0){  //JUGADOR 1
+            if(k==0){
+                printf("Turno del jugador 1\n");
+                int opc = moverJugador(tablero,pos,id);
+                k=1;
+                if(opc==1){
+                    write(ab[1],"1",1);    
+                }
+                else if(opc==2){ //se salta un turno
+                    write(ab[1],"2",1);
+                }
+                else if(opc==0){
+                    break;
+                }
+            }
+            else{
+                read(da[0],leer,1);
+                if(strcmp(leer,"1\n")!=0){
+                    printf("Turno del jugador 1\n");
+                    int opc = moverJugador(tablero,pos,id);
+                    if(opc==1){
+                        write(ab[1],"1",1);    
+                    }
+                    else if(opc==2){ //se salta un turno
+                        write(ab[1],"2",1);
+                    }
+                    else if(opc==0){
+                        break;
+                    }
+                }
+                else if(strcmp(leer,"2\n")!=0){
+                    write(ab[1],"1",1);
+                }
+            }
+        }
+        else if(id==1){ //JUGADOR 2
+            read(ab[0],leer,1);
+            if(strcmp(leer,"1\n")!=0){
+                printf("Turno del jugador 2\n");
+                int opc = moverJugador(tablero,pos,id);
+                if(opc==1){
+                    write(bc[1],"1",1);    
+                }
+                else if(opc==2){ //se salta un turno
+                    write(bc[1],"2",1);
+                }
+                else if(opc==0){
+                    break;
+                }
+                
+            }
+            else if(strcmp(leer,"2\n")!=0){
+                write(bc[1],"1",1);
+            }
+        }
+        else if(id==2){ //JUGADOR 3
+            read(bc[0],leer,1);
+            if(strcmp(leer,"1\n")!=0){
+                printf("Turno del jugador 3\n");
+                int opc = moverJugador(tablero,pos,id);
+                if(opc==1){
+                    write(cd[1],"1",1);    
+                }
+                else if(opc==2){ //se salta un turno
+                    write(cd[1],"2",1);
+                }
+                else if(opc==0){
+                    break;
+                }
+                
+            }
+            else if(strcmp(leer,"2\n")!=0){
+                write(cd[1],"1",1);
+            }
+        }
+        else if(id==3){ //JUGADOR 4
+            read(cd[0],leer,1);
+            if(strcmp(leer,"1\n")!=0){
+                printf("Turno del jugador 4\n");
+                int opc = moverJugador(tablero,pos,id);
+                if(opc==1){
+                    write(da[1],"1",1);    
+                }
+                else if(opc==2){ //se salta un turno
+                    write(da[1],"2",1);
+                }
+                else if(opc==0){
+                    break;
+                }
+                
+            }
+            else if(strcmp(leer,"2\n")!=0){
+                write(da[1],"1",1);
+            }
+        }
+    }
+    printf("GANASTE WENA WN!!!\n");
     return 0;
 }
